0c7c10efec326d613f8f7e5025f3aceab7c75b64
suh-prog <suzancrodrigues_@hotmail.com>
2025-07-24 15:35:07 -0300

    Aula 6 - Operações Básicas

---
+++ Bolleans.js
@@ -0,0 +1,6 @@
+// Mini Quiz - Verdadeiro ou Falso
+
+console.log("1. 5 > 3:", 5 > 3); // true
+console.log("2. 10 == 5:", 10 == 5); // false
+console.log("3. !(5 < 10):", !(5 < 10)); // false
+console.log("4. 6 % 2 == 0 && 6 > 3:", 6 % 2 == 0 && 6 > 3); // true
