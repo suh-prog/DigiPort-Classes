1797f69f9845d4291fabc986a88d0942177649a0
suh-prog <suzancrodrigues_@hotmail.com>
2025-07-28 16:41:27 -0300

    Aula 4 - Constantes e Variáveis

---
+++ Aula 4/tipos.js
@@ -0,0 +1,13 @@
+//Declarar uma variável pra cada tipo
+
+let idade = 35;     //number
+let nome = "Suzan"; //string
+let ativo = true;   //boolean
+let nota;           //undefined
+let cidade = null;  //null
+
+console.log("idade:", idade);
+console.log("nome:", nome);
+console.log("ativo:", ativo);
+console.log("nota:", nota);
+console.log("cidade:", cidade);
