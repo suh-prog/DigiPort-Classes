29f08c92c81ba48048b8c9987c4dbe9a9acd2621
suh-prog <suzancrodrigues_@hotmail.com>
2025-07-28 16:38:44 -0300

    Constantes e Variáveis (prática)

---
+++ Aula 5/stringsExemplo.js
@@ -0,0 +1,21 @@
+//Crie uma variável com a string "Javascript é incrível!"
+let minhaString = "Javascript é incrível!"
+
+//Acesse e imprima a quinta letra da string
+console.log(minhaString[4])
+
+//Use charAt() para acessar o mesmo caractere
+console.log(minhaString.charAt(4))
+
+//Substitua a palavra ïncrivel" por "poderoso"
+console.log(minhaString.replace("incrível", "poderoso"))
+
+//Imprime o número de caracteres da string
+console.log(minhaString.length)
+
+//Use slice() para extrair a palavra "Javascript"
+console.log(minhaString.slice(0, 10))
+
+//Use concat() para adicionar "Vamos aprender!" ao final da string
+console.log(minhaString.concat(" Vamos aprender!"))
+
